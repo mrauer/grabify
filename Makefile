9edcf28b3d8a20eb1baeba3f70d568337d0e3aa0
Maxime Rauer <maxime.rauer@gmail.com>
2021-07-10 20:27:37 -0500

    [MID] Get from YouTube

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	docker build -t grabity:latest .
 
 dev:
-	docker build -t grabity:latest . && docker run -it --rm -v ${CURDIR}:/usr/src/app/go/src/github.com/mrauer/grabity grabity:latest && docker exec -it grabity:latest
+	docker build -t grabity:latest . && docker run --env-file .env -it --rm -v ${CURDIR}:/usr/src/app/go/src/github.com/mrauer/grabity grabity:latest && docker exec -it grabity:latest
 
 binary:
 	env GOOS=linux GOARCH=amd64 go build -i -o grabity
--- main.go
+++ main.go
@@ -1,9 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
+
+const (
+	YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=%s&key=%s"
+	TIMEOUT_SECONDS = 2
+)
+
+type items struct {
+	Id struct {
+		VideoId string `json:"videoId"`
+	}
+}
+
+type result struct {
+	Items []items `json:"items"`
+}
 
 func main() {
 	fmt.Println("Grabity")
+
+	query := "how+bizarre"
+
+	url := fmt.Sprintf(YOUTUBE_API_URL, query, os.Getenv("YOUTUBE_API_KEY"))
+
+	spaceClient := http.Client{
+		Timeout: time.Second * TIMEOUT_SECONDS, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	resp := result{}
+	jsonErr := json.Unmarshal(body, &resp)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	fmt.Println(resp.Items)
 }
